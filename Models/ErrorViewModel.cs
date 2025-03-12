f966382c3866e2c31fb1a422c1b45b3985e1a811
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-03-12 13:32:26 +0100

    initiated webapp project for the outdoorsy company

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OutdoorsyCloudyMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
